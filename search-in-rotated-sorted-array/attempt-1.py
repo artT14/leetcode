baf2846e4c8c0677273533eff0aee7b0f6ee6198
artT14 <lamferme@gmail.com>
2022-12-18 23:22:59 -0800

    search-in-rotated-sorted-array

--- search-in-rotated-sorted-array/attempt-1.py
+++ search-in-rotated-sorted-array/attempt-1.py
@@ -21,36 +21,27 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        # STEP 1: FIND PIVOT POINT
-		# STEP 2: DO BINARY SEARCH USING PIVOT POINT
-        length = len(nums)
-        low = 0
-        high = length - 1
-        mid = (high+low)//2
-        PIVOT = None
-        while PIVOT is None:
-            print(low,mid,high)
-            if nums[low] < nums[high]:
-                PIVOT = low
-            elif (high - mid <= 1) and (mid - low <= 1):
-                check_low = min(0, nums[mid] - nums[low])
-                check_high = min(0, nums[mid] - nums[high])
-                if check_low and check_high:
-                    PIVOT = high
-                if not check_low and not check_high:
-                    PIVOT = mid
-                if check_low and not check_high:
-                    PIVOT = low
-            if nums[low] > nums[mid] and nums[mid] < nums[high]:
-                high = mid
-                mid = (high+low)//2
-            if nums[low] < nums[mid] and nums[mid] > nums[high]:
-                low = mid
-                mid = (high+low)//2
-        return PIVOT
+        if not nums: return -1
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            
+            if nums[mid] == target:
+                return mid
+            
+            if nums[left] <= nums[mid]:
+                if nums[left] <= target <=nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            else:
+                if nums[mid] <= target <=nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+                    
+        return -1
 
-sol = Solution().search([4,5,6,7,0,1,2,3],0)
+sol = Solution().search([6,7,8,9,10,0,1,2,3,4,5],0)
 print(sol)
-
-# OUT OF TIME
-# CHECK SOLUTION https://leetcode.com/problems/search-in-rotated-sorted-array/discuss/2595571/my-solution
