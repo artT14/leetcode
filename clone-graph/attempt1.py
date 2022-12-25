0cbbf0d440619869b1b985a599c38e69558aa0d7
artT14 <lamferme@gmail.com>
2022-12-24 21:20:12 -0800

    Progress clone-graph

--- clone-graph/attempt1.py
+++ clone-graph/attempt1.py
@@ -9,11 +9,12 @@ Each node in the graph contains a value (int) and a list (List[Node]) of its nei
 
 """
 # Definition for a Node.
+# """
 class Node(object):
     def __init__(self, val = 0, neighbors = None):
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
-"""
+
 
 class Solution(object):
     def cloneGraph(self, node):
@@ -21,3 +22,18 @@ class Solution(object):
         :type node: Node
         :rtype: Node
         """
+        hm={}
+        stack=[node]
+        head = node.val
+        while len(stack) > 0:
+            curr = stack.pop(0)
+            if curr.val in hm:
+                continue
+            hm[curr.val] = Node(val=curr.val)
+            for neighbor in curr.neighbors:
+                if neighbor.value in hm:
+                    hm[neighbor.value].neighbors.append(hm[curr.val])
+                else:
+                    stack.append(neighbor)
+        return hm[head]
+        #WRONG ANSWER
