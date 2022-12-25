4a75f7b9dbc23462186bdcfc8c8b27c15eaad897
artT14 <lamferme@gmail.com>
2022-12-24 16:30:54 -0800

    Update TODO

---
+++ longest-increasing-subsequence/attempt3.py
@@ -0,0 +1,13 @@
+"""
+https://leetcode.com/problems/longest-increasing-subsequence/
+Given an integer array nums, return the length of the longest strictly increasing 
+subsequence.
+"""
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # THERE IS A SOLUTION using BINSEARCH that converges in O(nlogn)
+        # DO THIS DURING YOUR FREE TIME
