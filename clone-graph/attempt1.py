bc43a43c2de43bca7947ae31a83890549af7d323
artT14 <lamferme@gmail.com>
2022-12-24 18:23:56 -0800

    Start clone-graph

---
+++ clone-graph/attempt1.py
@@ -0,0 +1,23 @@
+"""
+https://leetcode.com/problems/clone-graph/
+Given a reference of a node in a connected undirected graph.
+
+Return a deep copy (clone) of the graph.
+
+Each node in the graph contains a value (int) and a list (List[Node]) of its neighbors.
+"""
+
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution(object):
+    def cloneGraph(self, node):
+        """
+        :type node: Node
+        :rtype: Node
+        """
