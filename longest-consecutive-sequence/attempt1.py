bb63a8529d4c3b1e7308c3288aa05a476943459d
ArtT14 <lamferme@gmail.com>
2023-01-05 03:58:31 -0800

    longest consequtive sequence

---
+++ longest-consecutive-sequence/attempt1.py
@@ -0,0 +1,31 @@
+"""
+https://leetcode.com/problems/longest-consecutive-sequence/
+Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+
+You must write an algorithm that runs in O(n) time.
+"""
+from heapq import heapify, heappush, heappop
+
+class Solution(object):
+    def longestConsecutive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums: return 0
+        heap = []
+        heapify(heap)
+        for num in nums:
+            heappush(heap,num)
+        last = heappop(heap)
+        count = [1]
+        while heap:
+            curr = heappop(heap)
+            if curr == last+1:
+                count[-1] += 1
+            elif curr == last:
+                continue
+            else:
+                count.append(1)
+            last=curr
+        return max(count)
