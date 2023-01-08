685452c10da6cce34b162e497d2f293cdffa9ff0
artT14 <lamferme@gmail.com>
2023-01-07 18:37:58 -0800

    word-break

--- word-break/attempt1.py
+++ word-break/attempt1.py
@@ -11,3 +11,13 @@ class Solution(object):
         :type wordDict: List[str]
         :rtype: bool
         """
+        # bottom up Dynamic Programming 
+        dp = [False] * (len(s) + 1) # initialize all to False, default case
+        dp[len(s)] = True # Base case True, if we every get to it, then we are able to use dict to segment string
+        for i in range(len(s)-1,-1,-1): #traverse every index in reverse
+            for w in wordDict: # at most consider every word
+                if (i + len(w)) <= len(s) and s[i : i + len(w)] == w: # if within boundaries and there is a word match
+                    dp[i] = dp[i + len(w)] # update value based on past cases
+                if dp[i]: # if we have a vlaue that's True, no need to consider other words, break loop
+                    break
+        return dp[0] # dp[0] should give us final answer
