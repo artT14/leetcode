d5d389909be90e31b699111b977e62d396a930be
artT14 <lamferme@gmail.com>
2023-01-07 17:04:05 -0800

    Start word-break

---
+++ word-break/attempt1.py
@@ -0,0 +1,13 @@
+"""
+https://leetcode.com/problems/word-break/
+Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+Note that the same word in the dictionary may be reused multiple times in the segmentation.
+"""
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: bool
+        """
