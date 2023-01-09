ee1556265898f88bc1810769cb3f512eba69b0c3
artT14 <lamferme@gmail.com>
2023-01-08 22:12:48 -0800

    longest-palindromic-substring

--- longest-palindromic-substring/attempt1.py
+++ longest-palindromic-substring/attempt1.py
@@ -1,9 +1,5 @@
 """
-Given a string s, return the longest 
-palindromic
- 
-substring
- in s.
+Given a string s, return the longest palindromic substring in s.
 """
 class Solution(object):
     def longestPalindrome(self, s):
@@ -11,3 +7,22 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
+        result = ""
+        result_length = 0
+        
+        for i in range(len(s)):
+            l,r = i,i
+            while l >= 0 and r < len(s) and s[r] == s[l]: # check odds
+                if(r-l+1 > result_length):
+                    result = s[l : r+1]
+                    result_length = len(result)
+                r += 1
+                l -= 1
+            l,r = i, i+1
+            while l >= 0 and r < len(s) and s[r] == s[l]: # check evens
+                if(r-l+1 > result_length):
+                    result = s[l : r+1]
+                    result_length = len(result)
+                r += 1
+                l -= 1
+        return result
