1b963a0c4e2a52026ea85a1ee28a32cf06d8f844
artT14 <lamferme@gmail.com>
2023-01-08 22:24:42 -0800

    palindromic-substrings

---
+++ palindromic-substrings/attempt1.py
@@ -0,0 +1,29 @@
+"""
+Given a string s, return the number of palindromic substrings in it.
+
+A string is a palindrome when it reads the same backward as forward.
+
+A substring is a contiguous sequence of characters within the string.
+"""
+class Solution(object):
+    def countSubstrings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        count = 0
+        for i in len(s):
+            r, l = i, i
+            # odd palindrome
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                count += 1
+                r += 1
+                l -= 1
+            # even palindrome
+            r, l = i+1, i
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                count += 1
+                r += 1
+                l -= 1
+        return count
+        #ACCEPTED
